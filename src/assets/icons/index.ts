4e0a6acd7ad25b652590ce32dca4ef544455f5b0
Kirill <mustang.horse34@gmail.com>
2024-02-16 12:28:49 +0700

    add assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as SearchIcon } from './search.svg?react';
+export { default as CartIcon } from './cart.svg?react';
+export { default as LoaderIcon } from './loader.svg?react';
+export { default as RedCollarIcon } from './redCollar.svg?react';
+export { default as CloseIcon } from './close.svg?react';
