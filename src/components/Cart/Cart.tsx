8a008a7ad83f83b7837e6c39961cdf71c1f489c6
Kirill <mustang.horse34@gmail.com>
2024-02-16 14:37:12 +0700

    add Cart Modal component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,15 +1,48 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
-import { CartIcon } from 'src/assets/icons';
+import { CartIcon, CloseIcon } from 'src/assets/icons';
 
 import styles from './Cart.module.scss';
 
 const Cart: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openCart = () => {
+    setIsOpen(true);
+  };
+
+  const closeCart = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={styles.cart}>
-      <CartIcon />
-      <p>cart</p>
-    </div>
+    <>
+      <button onClick={openCart} type='button' className={styles.cart}>
+        <CartIcon />
+        <p>cart</p>
+      </button>
+      {isOpen && (
+        <div onClick={closeCart} className={styles.modal}>
+          <div onClick={(e) => e.stopPropagation()} className={styles.content}>
+            <div className={styles.header}>
+              <div className={styles.cart}>
+                <CartIcon />
+                <p>cart</p>
+              </div>
+              <CloseIcon onClick={closeCart} className={styles.closeIcon} />
+            </div>
+            <p className={styles.empty}>cart is empty :(</p>
+            <button
+              onClick={closeCart}
+              type='button'
+              className={styles.backButton}
+            >
+              back to products
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
