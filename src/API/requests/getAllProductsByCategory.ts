ccbbf7ab5bd3ebbed61363ef14aa939a5b975483
Kirill <mustang.horse34@gmail.com>
2024-02-17 08:40:03 +0700

    add request getAllProductsByCategory

---
+++ src/API/requests/getAllProductsByCategory.ts
@@ -0,0 +1,11 @@
+import BASE_URI from 'src/API/BASE_URI';
+
+const getAllProductsByCategory = async (category: string) => {
+  const res = await fetch(`${BASE_URI}products/category/${category}`);
+
+  const data = res.json();
+
+  return data;
+};
+
+export default getAllProductsByCategory;
