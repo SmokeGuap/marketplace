e7320aae3bff060db6039f2b9c48b4f7ff84552c
Kirill <mustang.horse34@gmail.com>
2024-02-17 11:25:56 +0700

    some fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/marketplace/'
+  base: '/marketplace/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
